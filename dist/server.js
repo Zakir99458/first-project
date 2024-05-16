dbc104676eff0ce4d336db4dc989ff12007b3c9d
Mohammad Z. Hossain (Nokia) <mohammad.z.hossain@nokia.com>
2024-05-16 21:40:41 +0300

    js

--- dist/server.js
+++ dist/server.js
@@ -8,12 +8,25 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const port = 3000;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const app_1 = __importDefault(require("./app"));
+const config_1 = __importDefault(require("./app/config"));
+// const port = 3000
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield mongoose.connect('process.env.DATABSE_URL');
+        yield mongoose_1.default.connect(config_1.default.database_url);
+        try {
+            app_1.default.listen(config_1.default.port, () => {
+                console.log(`Example app listening on port ${config_1.default.port}`);
+            });
+        }
+        catch (err) {
+            console.log(err);
+        }
     });
 }
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+main();
